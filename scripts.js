28223398e3211f109f0d1c37cd405e3710e86145
Jitesh <jiteshs855@gmail.com>
2021-12-04 02:37:53 +0530

    complete with onclick feature

--- scripts.js
+++ scripts.js
@@ -1,8 +1,43 @@
-var btn = document.getElementsByClassName("collapse-button");
-var ans = document.getElementsByClassName("answer");
+// var btn = document.getElementsByClassName("collapse-button");
+// var ans = document.getElementsByClassName("answer");
 
-for (let i = 0; i < btn.item.length; i++) {
-  btn[i].onclick = function () {
-    ans[i].classList.toggle(".active");
-  };
-}
+// for (let i = 0; i < btn.item.length; i++) {
+//   btn[i].onclick = function () {
+//     ans[i].classList.toggle(".active");
+//   };
+// }
+// let btn = $(".collapse-button");
+// for (let i = 0; i < 5; i++) {
+// $(".collapse-button").each(clicker);
+// .click(function () {
+//   // $(".answer")[0].slideToggle();
+//   $("#heading").css("display", "none");
+// });
+
+// }
+// function clicker() {
+//   $(".collapse-button").click(function () {
+//     $(".answer").slideToggle();
+//   });
+// }
+// $(".card-text")
+//   .children()
+//   .click(function () {
+//     $(".answer").slideToggle();
+//   });
+
+$(".collapse-button1").click(function () {
+  $(".answer1").slideToggle();
+});
+$(".collapse-button2").click(function () {
+  $(".answer2").slideToggle();
+});
+$(".collapse-button3").click(function () {
+  $(".answer3").slideToggle();
+});
+$(".collapse-button4").click(function () {
+  $(".answer4").slideToggle();
+});
+$(".collapse-button5").click(function () {
+  $(".answer5").slideToggle();
+});
