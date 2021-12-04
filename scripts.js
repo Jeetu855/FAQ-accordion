925266a322ee060778f41ab92b0b6a2020f4245a
Jitesh <jiteshs855@gmail.com>
2021-12-04 13:31:09 +0530

    make question bold on click

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
 $(".collapse-button1").click(function () {
   $(".answer1").slideToggle();
   $(".svgg1").toggleClass("rotate");
+  $(".question1").toggleClass("bold");
 });
 $(".collapse-button2").click(function () {
   $(".svgg2").toggleClass("rotate");
   $(".answer2").slideToggle();
+  $(".question2").toggleClass("bold");
 });
 $(".collapse-button3").click(function () {
   $(".svgg3").toggleClass("rotate");
   $(".answer3").slideToggle();
+  $(".question3").toggleClass("bold");
 });
 $(".collapse-button4").click(function () {
   $(".svgg4").toggleClass("rotate");
   $(".answer4").slideToggle();
+  $(".question4").toggleClass("bold");
 });
